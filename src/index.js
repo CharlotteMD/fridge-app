147fea5cfc378f7821b4d4fb356c68a7cb64b699
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2021-01-13 15:16:41 +0000

    Move config

--- src/index.js
+++ src/index.js
@@ -8,24 +8,14 @@ import firebase from 'firebase/app';
 import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
 
+import {firebaseConfig} from './config';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAbKO_3eXgohlokz0KLT9EKTcrlq1RsDmY",
-  authDomain: "recipes-in-my-fridge.firebaseapp.com",
-  databaseURL: "https://recipes-in-my-fridge-default-rtdb.firebaseio.com",
-  projectId: "recipes-in-my-fridge",
-  storageBucket: "recipes-in-my-fridge.appspot.com",
-  messagingSenderId: "168202825457",
-  appId: "1:168202825457:web:c157f2d58f72d9cb334350",
-  measurementId: "G-HSJT4Z3TQ5"
-};
 
 firebase.initializeApp(firebaseConfig)
 // const database = firebase.database();
